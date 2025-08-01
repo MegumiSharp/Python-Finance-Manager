99a2bef434df1eeb2691f96f3137d9c3ceeabe2e
Simone <simone.enselmi@gmail.com>
2025-08-01 10:53:07 +0200

    Fix themes position folder

--- src/views/base_view.py
+++ src/views/base_view.py
@@ -1,15 +1,7 @@
 from abc import ABC, abstractmethod
 import customtkinter as ctk
 
-'''
-Base class for all views in the application
-It provides a common interface and basic functionality for all views
-Each view should inherit from this class and implement the setup_ui method
-This class also provides methods to show and hide the view
-It uses customtkinter for the UI components
-and is designed to be used with a controller for navigation between views
-The user parameter is used to access user settings and preferences 
-'''       
+'''Base class for all views in the application. It provides a common interface and basic functionality for all views'''       
 class BaseView(ctk.CTkFrame, ABC):
     def __init__(self, parent, controller=None, user=None):
         super().__init__(parent)
